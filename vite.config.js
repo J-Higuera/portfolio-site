68f3ef95963a78427b57d08a5d145e36bfdeb5ce
J-Higuera <juan.higuera2035@gmail.com>
2025-07-19 14:54:55 -0700

    #	modified:   index.html #	renamed:    public/videos/Movie Assistant Preview.webm -> public/videos/MoviePreview2.webm #	renamed:    public/videos/Movie_assistant_preview.mp4 -> public/videos/Moviepreview1.mp4 #	new file:   public/videos/movie_preview.png #	new file:   vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    // You can customize base here if needed
+    base: '/',  // this is the default, you can change if needed
+});
