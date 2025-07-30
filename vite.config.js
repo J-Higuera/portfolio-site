79736a0a0c4652663b48e3799efe50aec9ef07af
J-Higuera <juan.higuera2035@gmail.com>
2025-07-30 16:27:46 -0700

    #	renamed:    css/about.css -> public/css/about.css #	renamed:    css/contact.css -> public/css/contact.css #	renamed:    css/index.css -> public/css/index.css #	renamed:    css/nav.css -> public/css/nav.css #	renamed:    css/preloader.css -> public/css/preloader.css #	renamed:    css/project1.css -> public/css/project1.css #	renamed:    css/projects.css -> public/css/projects.css #	renamed:    css/social-links.css -> public/css/social-links.css #	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
     base: '/',
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                projects: resolve(__dirname, 'public/pages/projects.html'),
-                about: resolve(__dirname, 'public/pages/about.html'),
-                contact: resolve(__dirname, 'public/pages/contact.html')
-            }
-        }
-    }
 });
