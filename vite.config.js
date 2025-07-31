20736cbd95e683c51a8875f7f2e2032f6f18ab72
J-Higuera <juan.higuera2035@gmail.com>
2025-07-30 21:19:01 -0700

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,12 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                projects: resolve(__dirname, '/pages/movie_project.html'),
-                about: resolve(__dirname, '/pages/about.html'),
-                contact: resolve(__dirname, '/pages/contact.html'),
-                project2: resolve(__dirname, '/pages/2nd_project.html'),
-                project3: resolve(__dirname, '/pages/3rd_project.html'),
-                project4: resolve(__dirname, '/pages/4th_project.html')
+                projects: resolve(__dirname, 'pages/movie_project.html'),
+                about: resolve(__dirname, 'pages/about.html'),
+                contact: resolve(__dirname, 'pages/contact.html'),
+                project2: resolve(__dirname, 'pages/2nd_project.html'),
+                project3: resolve(__dirname, 'pages/3rd_project.html'),
+                project4: resolve(__dirname, 'pages/4th_project.html')
             }
         }
     }
