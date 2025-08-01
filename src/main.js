2916ded9ae387b99b3e5a68206b1a078e810b387
J-Higuera <juan.higuera2035@gmail.com>
2025-07-31 17:44:35 -0700

    modified:   src/main.js #

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // Import other site scripts
-import './about.js';
-import './contact.js';
 import './index.js';
 import './nav.js';
 import './projects.js';
 import './scroll.js';
 import './theme.js';
+import './about.js';
+import './contact.js';
 import './react-entry.jsx';
 
 
