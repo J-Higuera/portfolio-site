d8a88c9e6ac12bb712a5273b75022ca069985580
J-Higuera <juan.higuera2035@gmail.com>
2025-08-02 05:07:19 -0700

    #	modified:   index.html #	new file:   public/images/body/cat.jpg #	deleted:    public/images/body/tim.webp #	modified:   src/react-entry.jsx #

--- src/react-entry.jsx
+++ src/react-entry.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 
 document.addEventListener("DOMContentLoaded", () => {
-    requestIdleCallback(() => {
-        import("./react/GitHubCalendar.jsx").then(({ default: GitHubCalendar }) => {
-            const root = createRoot(document.getElementById("github-root"));
-            root.render(<GitHubCalendar />);
-        });
-    });
+    const target = document.getElementById("github-root");
+
+    if (!target) return;
+
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            const entry = entries[0];
+            if (entry.isIntersecting) {
+                requestIdleCallback(() => {
+                    import("./react/GitHubCalendar.jsx").then(({ default: GitHubCalendar }) => {
+                        const root = createRoot(target);
+                        root.render(<GitHubCalendar />);
+                    });
+                });
+
+                observer.unobserve(entry.target);
+            }
+        },
+        { threshold: 0.1 }
+    );
+
+    observer.observe(target);
 });
