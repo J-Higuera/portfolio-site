c4bc87b65e0fa912ad459a3c9766d5e33f1634a8
J-Higuera <juan.higuera2035@gmail.com>
2025-08-02 19:12:47 -0700

    #	modified:   css/about.css #	modified:   src/contact.js #	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -10,5 +10,6 @@ import './react-entry.jsx';
 import '../css/index.css';
 import '../css/projects.css';
 import '../css/about.css';
+import '../css/contact.css';
 import '../css/nav.css';
 import '../css/social-links.css';
